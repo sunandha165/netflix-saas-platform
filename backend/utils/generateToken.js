338cabe9ba55c0937857bee94e7ac0954f118f43
Sunandha Mani Vajrapu <vajrapusunandhamani@gmail.com>
2026-05-13 16:09:02 +0530

    Fixed production auth cookies

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,15 +1,34 @@
 import jwt from "jsonwebtoken";
 import { ENV_VARS } from "../config/envVars.js";
 
-export const generateTokenAndSetCookie = (userId, res) => {
-	const token = jwt.sign({ userId }, ENV_VARS.JWT_SECRET, { expiresIn: "15d" });
+export const generateTokenAndSetCookie = (
+	userId,
+	res
+) => {
 
-	res.cookie("jwt-netflix", token, {
-	httpOnly: true,
-	secure: true,
-	sameSite: "none",
-	maxAge: 15 * 24 * 60 * 60 * 1000,
-});
+	const token = jwt.sign(
+		{ userId },
+		ENV_VARS.JWT_SECRET,
+		{
+			expiresIn: "15d",
+		}
+	);
+
+	res.cookie(
+		"jwt-netflix",
+		token,
+		{
+			httpOnly: true,
+			secure: true,
+			sameSite: "none",
+			maxAge:
+				15 *
+				24 *
+				60 *
+				60 *
+				1000,
+		}
+	);
 
 	return token;
 };
